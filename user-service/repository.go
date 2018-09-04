dde01b89fe6032a42f45a2d36289ace0e3edf1a9
Tomasz Wlodarczyk <twl@phase-ii.com>
2018-09-04 16:44:49 +0100

    Third two

---
+++ user-service/repository.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"github.com/jinzhu/gorm"
+	pb "github.com/tomekwlod/microservices/user-service/proto/user"
+)
+
+// UserRepository defining here that we're going to be using the gorm in this repo
+type UserRepository struct {
+	db *gorm.DB
+}
+
+// Repository exposing the repository methods
+type Repository interface {
+	GetAll() ([]*pb.User, error)
+	Get(id string) (*pb.User, error)
+	Create(user *pb.User) error
+	GetByEmailAndPassword(user *pb.User) (*pb.User, error)
+}
+
+func (repo *UserRepository) GetAll() ([]*pb.User, error) {
+	var users []*pb.User
+	if err := repo.db.Find(&users).Error; err != nil {
+		return nil, err
+	}
+	return users, nil
+}
+
+func (repo *UserRepository) Get(id string) (*pb.User, error) {
+	var user *pb.User
+	user.Id = id
+	if err := repo.db.First(&user).Error; err != nil {
+		return nil, err
+	}
+	return user, nil
+}
+
+func (repo *UserRepository) GetByEmailAndPassword(user *pb.User) (*pb.User, error) {
+	if err := repo.db.First(&user).Error; err != nil {
+		return nil, err
+	}
+	return user, nil
+}
+
+func (repo *UserRepository) Create(user *pb.User) error {
+	if err := repo.db.Create(user).Error; err != nil {
+		return err
+	}
+	return nil
+}
